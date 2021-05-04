a3c3c38354f556ef54ddb3bd1ece81a1f4ba8b56
Eric Lam <lam.eric.ck@gmail.com>
2021-05-04 11:09:09 -0700

    SAVEPOINT

--- online_assessments/template.c
+++ online_assessments/template.c
@@ -10,9 +10,26 @@
 #include <stdlib.h>
 #include <string.h>
 
-
 int main() {
-    // printf("%d\n",); // print int
-    // printf("%ld\n",); // print long
+    /* 1D Array */
+    // int* arr = (int *)malloc(sizeof(int)*arr_size);
 
+    /* 2D Array */
+    // int **arr = (int **)malloc(arr_row_size * sizeof(int *));
+    // for (int i = 0; i < arr_row_size; i++) {
+    //     arr[i] = (int *)malloc(arr_col_size * sizeof(int));
+    // }
+
+    // for (int i = 0; i < arr_row_size; i++) {
+    //     int *curr = arr[i];
+    //     free(curr);
+    // }
+    // free(arr);
+
+    // printf("%d\n",a); // print int
+    // printf("%ld\n",); // print long
+    // printf("%f\n",); // print float
+    // printf("%e\n",); // print double
+    // printf("%c\n",); // print char
+    // printf("%s\n",); // print string
 }
